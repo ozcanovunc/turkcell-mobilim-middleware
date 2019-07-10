e6e4dd7229f56e6b38dc0546ea305bb55db4b357
ozcanovunc <ozcanovunc@outlook.com>
2019-07-10 12:46:42 +0000

    feat: Implement hotel retrieving service

---
+++ endpoints/hotel.js
@@ -0,0 +1,15 @@
+const request = require("../lib/request");
+const getEntities = require("../lib/getEntities");
+const genericErrorHandler = require("../lib/genericErrorHandler");
+
+module.exports = function(service) {
+    service.get('/mobile/hotel/:cityId', function(req, res) {
+        const { cityId } = req.params;
+        request("OtelListWithCityId", { cityId })
+            .then(e => {
+                let hotels = getEntities(e, "OtelBasicEntity");
+                res.status(200).json(hotels).end();
+            })
+            .catch(e => genericErrorHandler(e, res));
+    });
+};
---
+++ sample/hotel.sh
@@ -0,0 +1,44 @@
+curl --request GET localhost:80/mobile/hotel/127
+
+# Returns:
+# [{
+#     "District": "ÇANKAYA",
+#     "OtelId": 2,
+#     "OtelName": "Ankara Hiltonsa"
+# }, {
+#     "District": "SÖĞÜTÖZÜ",
+#     "OtelId": 20,
+#     "OtelName": "Wyndham Ankara"
+# }, {
+#     "District": "ÇANKAYA",
+#     "OtelId": 22,
+#     "OtelName": "Swissotel Ankara"
+# }, {
+#     "District": "YENİMAHALLE",
+#     "OtelId": 107,
+#     "OtelName": "Movenpick Hotel Ankara"
+# }, {
+#     "District": "GİMAT",
+#     "OtelId": 347,
+#     "OtelName": "Hilton Garden Inn Ankara Gimat (Ankara)"
+# }, {
+#     "District": "ÇANKAYA",
+#     "OtelId": 348,
+#     "OtelName": "Point Hotel Ankara"
+# }, {
+#     "District": "Ostim",
+#     "OtelId": 406,
+#     "OtelName": "Ostimpark Business Hotel (Ankara)"
+# }, {
+#     "District": "Yenimahalle",
+#     "OtelId": 434,
+#     "OtelName": "Crowne Plaza"
+# }, {
+#     "District": "Ulus",
+#     "OtelId": 437,
+#     "OtelName": "Radisson Blu Ankara"
+# }, {
+#     "District": "",
+#     "OtelId": "DIGER",
+#     "OtelName": "Diğer"
+# }]
