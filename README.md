4b242c2f6afa9eb0c6862537f4535f92a8be90a1
ozcanovunc <ozcanovunc@outlook.com>
2019-07-11 13:46:21 +0000

    docs: Add Readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # turkcell-poc-middleware
+
+## Installation
+
+#### With Docker
+
+- Install Docker
+- Run the following commands
+
+```
+docker build . -t turkcell/middleware
+docker run -p 80:80 turkcell/middleware
+```
+
+#### Without Docker
+
+- Install node v10
+- Run the following commands
+
+```
+npm i
+npm run start
+```
+
+## Usage
+
+- Go to the following link through your browser
+
+```
+localhost:80
+```
+- See the screen down below
+
+<img src="resources/main.png" width="300">
--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const PORT = 80;
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const pickEndpoints = require("./lib/pickEndpoints");
 const endpoints = require("./endpoints");
+const { PORT, GENERIC_SUCCESS_CODE } = require("./lib/constants");
 
 app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
    res
-      .status(200)
+      .status(GENERIC_SUCCESS_CODE)
       .send('<title>Turkcell</title>' +
          '<center><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTSAxCVXwC-y6LSdUTcxZZkL1Mvxo_cDpDMv_U_fMbiDdce9D2A" ></img> ' +
-         '<h1>Turkcell Poc Middleware is up and running...</h1> <p>' + req.headers.cookie + '</p></center>').end();
+         '<h1 style="font-family:sans-serif;">Turkcell Mobilim Middleware is up and running...</h1></center>').end();
 });
 
-app.listen(PORT, () => console.log(`Turkcell Poc Middleware listening on port ${PORT}!`));
+app.listen(PORT, () => console.log(`Turkcell Mobilim Middleware listening on port ${PORT}!`));
 
 pickEndpoints(app, endpoints);
